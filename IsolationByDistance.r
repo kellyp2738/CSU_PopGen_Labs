851955455da197506d8d20a5be7e944e317c6f8f
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-10-01 09:29:47 -0600

    First commit.

---
+++ IsolationByDistance.r
@@ -0,0 +1,38 @@
+# Isolation by distance
+# tested on OSX 30 Sept 2015
+# key assmuption: the ordering of populations in the geo. and genetic distance matrices are preserved
+
+library(StAMPP)
+
+# load and manipulate the geographic distance
+linear<-read.csv('~/Dropbox/BZ 577/Week 5-6/FigsData/linear.csv')
+linear.mat<-as.matrix(linear[1:13, 2:14])
+linear.dist<-log(linear.mat[lower.tri(linear.mat)])
+
+# load and manipulate the genetic distance
+# data are automatically loaded as GENIND format
+pdogdata<-read.genepop('~/Dropbox/BZ 577/Week 5-6/FigsData/Cynomys population study.gen')
+
+# StAMPP needs genlight data... 
+pdog.gl<-new("genlight", gen=pdogdata@tab, ploidy=2,loc.names=pdogdata@all.names,
+             pop=pdogdata@pop,ind.names=seq(1,155,1))
+
+# calculate Fst matrix
+Fstats<-stamppFst(pdog.gl, nboots=10000)
+
+# pull out only the Fst values (also in this data structure are Pvalues and the Fst calculated for each bootstrap iteration)
+Fst.mat<-Fstats$Fsts #lower triangle only
+
+# extract the lower triangle
+Fst.vec<-Fst.mat[lower.tri(Fst.mat)]
+
+# linearize Fst
+Fst.norm<-Fst.vec/(1-Fst.vec)
+
+# visualize data
+plot(linear.dist, Fst.norm, xlab='log(Geographic Distance)', 
+     ylab='Genetic Distance', pch=16)
+
+# conduct the Mantel test (tell R to use the ecodist version)
+PdogMantel <- ecodist::mantel(Fst.norm ~ linear.dist, nperm = 1000)
+
